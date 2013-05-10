5f3044cb82a30f8083cd90ce9e8d327969e7f266
mogoodrich <mgoodrich@pih.org>
2013-05-10 13:02:09 -0400

    HTML-461: Cannot add 2 different users to the same encounterRole

--- api/src/main/java/org/openmrs/module/htmlformentry19ext/ProviderAndRoleElement.java
+++ api/src/main/java/org/openmrs/module/htmlformentry19ext/ProviderAndRoleElement.java
@@ -15,6 +15,7 @@ package org.openmrs.module.htmlformentry19ext;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -22,7 +23,6 @@ import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.openmrs.Encounter;
 import org.openmrs.EncounterRole;
 import org.openmrs.Person;
 import org.openmrs.Provider;
@@ -45,11 +45,12 @@ import org.springframework.util.StringUtils;
 public class ProviderAndRoleElement implements HtmlGeneratorElement, FormSubmissionControllerAction {
 
 	boolean required = false;
+    int count = 1;
 
 	EncounterRoleWidget roleWidget;
 	ErrorWidget roleErrorWidget;
-	ProviderWidget providerWidget;
-	ErrorWidget providerErrorWidget;
+	List<ProviderWidget> providerWidgets = new ArrayList<ProviderWidget>();
+	List<ErrorWidget> providerErrorWidgets = new ArrayList<ErrorWidget>();
 	
 	// in case EncounterRole is specified as a parameter to the tag
 	EncounterRole encounterRole;
@@ -59,8 +60,14 @@ public class ProviderAndRoleElement implements HtmlGeneratorElement, FormSubmiss
 	 * @throws BadFormDesignException 
      */
     public ProviderAndRoleElement(FormEntryContext context, Map<String, String> parameters) throws BadFormDesignException {
-    	if (parameters.containsKey("required"))
+    	if (parameters.containsKey("required")) {
     		required = Boolean.valueOf(parameters.get("required"));
+        }
+        if (parameters.containsKey("count")) {
+            count = Integer.valueOf(parameters.get("count"));
+        }
+
+        // if there is an encounterRole specified, use that; otherwise create a roleWidget
     	if (parameters.containsKey("encounterRole")) {
     		EncounterService es = Context.getEncounterService();
     		String param = parameters.get("encounterRole");
@@ -78,39 +85,66 @@ public class ProviderAndRoleElement implements HtmlGeneratorElement, FormSubmiss
     		context.registerWidget(roleWidget);
     		context.registerErrorWidget(roleWidget, roleErrorWidget);
     	}
-    	providerWidget = new ProviderWidget();
-    	providerErrorWidget = new ErrorWidget();
-    	context.registerWidget(providerWidget);
-    	context.registerErrorWidget(providerWidget, providerErrorWidget);
-    	
-    	boolean initialProviderSet = false;
-    	if (context.getExistingEncounter() != null) {
-    		if (encounterRole != null) {
-    			Set<Provider> byRole = context.getExistingEncounter().getProvidersByRole(encounterRole);
-    			if (byRole.size() == 1) {
-    				providerWidget.setInitialValue(byRole.iterator().next());
+
+        boolean initialProviderSet = false;
+
+        // handle the case where no encounterRole attribute is specified
+        if (encounterRole == null) {
+            if (count > 1) {
+                throw new BadFormDesignException("HTML Form Entry does not (yet) support multiple providers per encounter without specifying encounter roles");
+            }
+
+            ProviderWidget providerWidget = new ProviderWidget();
+            ErrorWidget providerErrorWidget = new ErrorWidget();
+            context.registerWidget(providerWidget);
+            context.registerErrorWidget(providerWidget, providerErrorWidget);
+            providerWidgets.add(providerWidget);
+            providerErrorWidgets.add(providerErrorWidget);
+
+            if (context.getExistingEncounter() != null) {
+                Map<EncounterRole, Set<Provider>> byRoles = context.getExistingEncounter().getProvidersByRoles();
+                if (byRoles.size() > 0) {
+                    // currently we only support a single provider in this mode
+                    if (byRoles.size() > 1 || byRoles.values().iterator().next().size() > 1) {
+                        throw new BadFormDesignException("HTML Form Entry does not (yet) support multiple providers per encounter if you don't specify an encounterRole for each of them");
+                    }
+
+                    Entry<EncounterRole, Set<Provider>> roleAndProvider = byRoles.entrySet().iterator().next();
+                    Provider p = roleAndProvider.getValue().iterator().next();
+                    providerWidget.setInitialValue(p);
+                    initialProviderSet = true;
+                    roleWidget.setInitialValue(roleAndProvider.getKey());
+                }
+            }
+        }
+        // handle the case where an encounter role is specified
+        else {
+
+            // get any existing providers for the specified role
+            List<Provider> byRole = null;
+            if (context.getExistingEncounter() != null) {
+                byRole = new ArrayList<Provider>(context.getExistingEncounter().getProvidersByRole(encounterRole));
+            }
+
+            // register the provider widgets, setting any existing provider values
+    	    for (int currentIteration = 0; currentIteration < count; currentIteration++) {
+
+                ProviderWidget providerWidget = new ProviderWidget();
+                ErrorWidget providerErrorWidget = new ErrorWidget();
+                context.registerWidget(providerWidget);
+                context.registerErrorWidget(providerWidget, providerErrorWidget);
+                providerWidgets.add(providerWidget);
+                providerErrorWidgets.add(providerErrorWidget);
+
+                if (byRole != null && byRole.size() > currentIteration) {
+    				providerWidget.setInitialValue(byRole.get(currentIteration));
     				initialProviderSet = true;
-    			} else if (byRole.size() > 1) {
-    				throw new BadFormDesignException("HTML Form Entry does not (yet) support multiple providers with the same encounter role");
-    			} 
-    		} else {
-    			Map<EncounterRole, Set<Provider>> byRoles = context.getExistingEncounter().getProvidersByRoles();
-    			if (byRoles.size() > 0) {
-        			// currently we only support a single provider in this mode
-        			if (byRoles.size() > 1 || byRoles.values().iterator().next().size() > 1) {
-        				throw new BadFormDesignException("HTML Form Entry does not (yet) support multiple providers per encounter if you don't specify an encounterRole for each of them");
-        			}
-        			
-    				Entry<EncounterRole, Set<Provider>> roleAndProvider = byRoles.entrySet().iterator().next();
-    				Provider p = roleAndProvider.getValue().iterator().next();
-    				providerWidget.setInitialValue(p);
-    				initialProviderSet = true;
-    				roleWidget.setInitialValue(roleAndProvider.getKey());
     			}
-    		}
-    	}
-    	
-    	if (!initialProviderSet && providerWidget != null && StringUtils.hasText(parameters.get("default"))) {
+            }
+        }
+
+        // set the default provider if no existing provider and a default is specified
+    	if (!initialProviderSet && providerWidgets.iterator().next() != null && StringUtils.hasText(parameters.get("default"))) {
     		String temp = parameters.get("default");
     		Provider provider = null;
     		if ("currentUser".equals(temp)) {
@@ -126,7 +160,7 @@ public class ProviderAndRoleElement implements HtmlGeneratorElement, FormSubmiss
 	    		}
     		}
     		if (provider != null) {
-    			providerWidget.setInitialValue(provider);
+    			providerWidgets.iterator().next().setInitialValue(provider);
     		}
     	}
     }
@@ -143,11 +177,15 @@ public class ProviderAndRoleElement implements HtmlGeneratorElement, FormSubmiss
     			ret.append(roleErrorWidget.generateHtml(context));
     		ret.append(": ");
     	}
-    	if (providerWidget != null) {
+
+        Iterator<ErrorWidget> errorWidgetIterator = providerErrorWidgets.iterator();
+        for (ProviderWidget providerWidget : providerWidgets) {
 			ret.append(providerWidget.generateHtml(context));
-			if (context.getMode() != Mode.VIEW)
-				ret.append(providerErrorWidget.generateHtml(context));
+			if (context.getMode() != Mode.VIEW) {
+				ret.append(errorWidgetIterator.next().generateHtml(context));
+            }
 		}
+
 	    return ret.toString();
     }
     
@@ -156,9 +194,12 @@ public class ProviderAndRoleElement implements HtmlGeneratorElement, FormSubmiss
      */
     @Override
     public Collection<FormSubmissionError> validateSubmission(FormEntryContext context, HttpServletRequest submission) {
-    	if (!required)
+
+        if (!required) {
     		return null;
-    	EncounterRole role = encounterRole;
+        }
+
+        EncounterRole role = encounterRole;
     	Provider provider = null;
     	List<FormSubmissionError> ret = new ArrayList<FormSubmissionError>();
     	if (roleWidget != null) {
@@ -166,10 +207,20 @@ public class ProviderAndRoleElement implements HtmlGeneratorElement, FormSubmiss
     		if (role == null)
         		ret.add(new FormSubmissionError(roleWidget, Context.getMessageSourceService().getMessage("htmlformentry.error.required")));
     	}
-    	if (providerWidget != null) {
-    		provider = (Provider) providerWidget.getValue(context, submission);
-    		if (provider == null)
-        		ret.add(new FormSubmissionError(providerWidget, Context.getMessageSourceService().getMessage("htmlformentry.error.required")));
+
+        boolean atLeastOneProviderSpecified = false;
+
+        for (ProviderWidget providerWidget : providerWidgets) {
+            if (providerWidget != null) {
+                provider = (Provider) providerWidget.getValue(context, submission);
+                if (provider != null) {
+                    atLeastOneProviderSpecified = true;
+                }
+            }
+        }
+
+    	if (!atLeastOneProviderSpecified) {
+            ret.add(new FormSubmissionError(providerWidgets.get(0), Context.getMessageSourceService().getMessage("htmlformentry.error.required")));
     	}
     	return ret;
     }
@@ -179,22 +230,32 @@ public class ProviderAndRoleElement implements HtmlGeneratorElement, FormSubmiss
      */
     @Override
     public void handleSubmission(FormEntrySession session, HttpServletRequest submission) {
-    	EncounterRole role = encounterRole;
+
+        EncounterRole role = encounterRole;
     	Provider provider = null;
-    	if (roleWidget != null) {
+
+        if (roleWidget != null) {
     		role = (EncounterRole) roleWidget.getValue(session.getContext(), submission);
     	}
-    	if (providerWidget != null) {
-    		provider = (Provider) providerWidget.getValue(session.getContext(), submission);
-    	}
-    	if (provider != null) {
-    		session.getSubmissionActions().getCurrentEncounter().setProvider(role, provider);
-    	} else if (role != null) {
-    		// role != null while provider == null is something like "Doctor: null", so clear providers from that role
-    		Encounter encounter = session.getSubmissionActions().getCurrentEncounter();  		
-    		for (Provider p : encounter.getProvidersByRole(role))
-    			encounter.removeProvider(role, p);
-    	}
-    }
 
+        Set<Provider> currentProvidersForRole = session.getSubmissionActions().getCurrentEncounter().getProvidersByRole(role);
+
+        for (ProviderWidget providerWidget : providerWidgets) {
+    		provider = (Provider) providerWidget.getValue(session.getContext(), submission);
+
+            if (provider != null) {
+                // if this provider it not currently one of the provider, add her
+                if (currentProvidersForRole != null || !currentProvidersForRole.contains(provider)) {
+                    session.getSubmissionActions().getCurrentEncounter().addProvider(role, provider);
+                }
+                // we remove this provider so that we end up with a list of providers to void
+                currentProvidersForRole.remove(provider);
+            }
+        }
+
+        // now remove any current providers that we haven't found in the provider list
+        for (Provider providerToRemove : currentProvidersForRole) {
+            session.getSubmissionActions().getCurrentEncounter().removeProvider(role, providerToRemove);
+        }
+    }
 }
--- api/src/test/java/org/openmrs/module/htmlformentry19ext/IntegrationTest.java
+++ api/src/test/java/org/openmrs/module/htmlformentry19ext/IntegrationTest.java
@@ -14,6 +14,7 @@
 package org.openmrs.module.htmlformentry19ext;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -59,7 +60,7 @@ public class IntegrationTest extends BaseModuleContextSensitiveTest {
 						
 			@Override
 			public String[] widgetLabels() {
-				return new String[] { "Date:", "Location:", "ProviderAndRole:", "ProviderAndRole:!!1" };
+				return new String[] { "Date:", "Location:", "ProviderAndRole:", "ProviderAndRole:!!1"};
 			}
 			
 			@Override
@@ -83,8 +84,92 @@ public class IntegrationTest extends BaseModuleContextSensitiveTest {
 			}
 		}.run();
 	}
-	
-	@Test
+
+    @Test
+    public void testTagWithRequiredAttribute() throws Exception {
+        final Date date = new Date();
+        new RegressionTestHelper() {
+
+            @Override
+            protected String getXmlDatasetPath() {
+                return "org/openmrs/module/htmlformentry19ext/include/";
+            }
+
+            @Override
+            public String getFormName() {
+                return "requiredEncounterProviderAndRoleTag";
+            }
+
+            @Override
+            public String[] widgetLabels() {
+                return new String[] { "Date:", "Location:", "ProviderAndRole:", "ProviderAndRole:!!1"};
+            }
+
+            @Override
+            public void setupRequest(MockHttpServletRequest request, Map<String, String> widgets) {
+                request.addParameter(widgets.get("Date:"), dateAsString(date));
+                request.addParameter(widgets.get("Location:"), "2");
+                request.addParameter(widgets.get("ProviderAndRole:"), "3"); // encounter role
+                request.addParameter(widgets.get("ProviderAndRole:!!1"), "2"); // provider
+            }
+
+            @Override
+            public void testResults(SubmissionResults results) {
+                results.assertNoErrors();
+                results.assertEncounterCreated();
+                results.assertLocation(2);
+                Map<EncounterRole, Set<Provider>> byRoles = results.getEncounterCreated().getProvidersByRoles();
+                Assert.assertEquals(1, byRoles.size());
+                EncounterRole encRole = byRoles.keySet().iterator().next();
+                Assert.assertEquals(Integer.valueOf(3), encRole.getEncounterRoleId());
+                Assert.assertEquals(Integer.valueOf(2), byRoles.get(encRole).iterator().next().getProviderId());
+            }
+        }.run();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testTagWithRequiredAttribute_shouldThrowExceptionIfNoProvider() throws Exception {
+        final Date date = new Date();
+        new RegressionTestHelper() {
+
+            @Override
+            protected String getXmlDatasetPath() {
+                return "org/openmrs/module/htmlformentry19ext/include/";
+            }
+
+            @Override
+            public String getFormName() {
+                return "requiredEncounterProviderAndRoleTag";
+            }
+
+            @Override
+            public String[] widgetLabels() {
+                return new String[] { "Date:", "Location:", "ProviderAndRole:", "ProviderAndRole:!!1"};
+            }
+
+            @Override
+            public void setupRequest(MockHttpServletRequest request, Map<String, String> widgets) {
+                request.addParameter(widgets.get("Date:"), dateAsString(date));
+                request.addParameter(widgets.get("Location:"), "2");
+                request.addParameter(widgets.get("ProviderAndRole:"), "3"); // encounter role
+            }
+
+            @Override
+            public void testResults(SubmissionResults results) {
+                results.assertNoErrors();
+                results.assertEncounterCreated();
+                results.assertLocation(2);
+                Map<EncounterRole, Set<Provider>> byRoles = results.getEncounterCreated().getProvidersByRoles();
+                Assert.assertEquals(1, byRoles.size());
+                EncounterRole encRole = byRoles.keySet().iterator().next();
+                Assert.assertEquals(Integer.valueOf(3), encRole.getEncounterRoleId());
+                Assert.assertEquals(Integer.valueOf(2), byRoles.get(encRole).iterator().next().getProviderId());
+            }
+        }.run();
+    }
+
+
+    @Test
 	public void testDefaultValue() throws Exception {
 		new RegressionTestHelper() {
 			
@@ -106,7 +191,7 @@ public class IntegrationTest extends BaseModuleContextSensitiveTest {
 	}
 	
 	@Test
-	public void testTagSpecifyingEncounterRoleTwice() throws Exception {
+	public void testTagSpecifyingEncounterProviderTwiceWithDifferentRoles() throws Exception {
 		final Date date = new Date();
 		new RegressionTestHelper() {
 			
@@ -117,7 +202,7 @@ public class IntegrationTest extends BaseModuleContextSensitiveTest {
 			
 			@Override
             public String getFormName() {
-				return "specifyingEncounterRoleTwice";
+				return "specifyingEncounterRoleTwiceWithDifferentRoles";
 			}
 						
 			@Override
@@ -160,5 +245,194 @@ public class IntegrationTest extends BaseModuleContextSensitiveTest {
 			
 		}.run();
 	}
-	
+
+    @Test
+    public void testTagSpecifyingEncounterProviderTwiceWithSameRole() throws Exception {
+        final Date date = new Date();
+        new RegressionTestHelper() {
+
+            @Override
+            protected String getXmlDatasetPath() {
+                return "org/openmrs/module/htmlformentry19ext/include/";
+            }
+
+            @Override
+            public String getFormName() {
+                return "specifyingEncounterRoleTwiceWithSameRole";
+            }
+
+            @Override
+            public String[] widgetLabels() {
+                return new String[] { "Date:", "Location:", "Doctors:", "Doctors:!!1" };
+            }
+
+            @Override
+            public void setupRequest(MockHttpServletRequest request, Map<String, String> widgets) {
+                request.addParameter(widgets.get("Date:"), dateAsString(date));
+                request.addParameter(widgets.get("Location:"), "2");
+                request.addParameter(widgets.get("Doctors:"), "2"); // Doctor Bob
+                request.addParameter(widgets.get("Doctors:!!1"), "1"); // Superuser  (hack to reference by widget id, but no label for this widget
+            }
+
+            @Override
+            public void testResults(SubmissionResults results) {
+                results.assertNoErrors();
+                results.assertEncounterCreated();
+                results.assertLocation(2);
+
+                Map<EncounterRole, Set<Provider>> byRoles = results.getEncounterCreated().getProvidersByRoles();
+                Assert.assertEquals(1, byRoles.size());
+
+                Set<Provider> doctors = byRoles.get(Context.getEncounterService().getEncounterRole(3));
+                Assert.assertEquals(2, doctors.size());
+
+                // we can't guarantee the order of providers, but make sure both providers are now present
+                Set<Integer> providerIds = new HashSet<Integer>();
+                for (Provider doctor : doctors) {
+                    providerIds.add(doctor.getId());
+                }
+                Assert.assertEquals(2, providerIds.size());
+                Assert.assertTrue(providerIds.contains(1));
+                Assert.assertTrue(providerIds.contains(2));
+            }
+
+            @Override
+            public boolean doViewEncounter() {
+                return true;
+            }
+
+            @Override
+            public void testViewingEncounter(Encounter encounter, String html) {
+                TestUtil.assertFuzzyContains("Doctor Bob", html);
+                TestUtil.assertFuzzyContains("Super User", html);
+            }
+
+        }.run();
+    }
+
+    @Test
+    public void testSpecifyingASingleProviderForTagThatAcceptsTwo() throws Exception {
+        final Date date = new Date();
+        new RegressionTestHelper() {
+
+            @Override
+            protected String getXmlDatasetPath() {
+                return "org/openmrs/module/htmlformentry19ext/include/";
+            }
+
+            @Override
+            public String getFormName() {
+                return "specifyingEncounterRoleTwiceWithSameRole";
+            }
+
+            @Override
+            public String[] widgetLabels() {
+                return new String[] { "Date:", "Location:", "Doctors:" };
+            }
+
+            @Override
+            public void setupRequest(MockHttpServletRequest request, Map<String, String> widgets) {
+                request.addParameter(widgets.get("Date:"), dateAsString(date));
+                request.addParameter(widgets.get("Location:"), "2");
+                request.addParameter(widgets.get("Doctors:"), "2"); // Doctor Bob
+            }
+
+            @Override
+            public void testResults(SubmissionResults results) {
+                results.assertNoErrors();
+                results.assertEncounterCreated();
+                results.assertLocation(2);
+
+                Map<EncounterRole, Set<Provider>> byRoles = results.getEncounterCreated().getProvidersByRoles();
+                Assert.assertEquals(1, byRoles.size());
+
+                Set<Provider> doctors = byRoles.get(Context.getEncounterService().getEncounterRole(3));
+                Assert.assertEquals(1, doctors.size());
+                Assert.assertEquals(new Integer(2), doctors.iterator().next().getId());
+            }
+
+            @Override
+            public boolean doViewEncounter() {
+                return true;
+            }
+
+            @Override
+            public void testViewingEncounter(Encounter encounter, String html) {
+                TestUtil.assertFuzzyContains("Doctor Bob", html);
+                TestUtil.assertFuzzyDoesNotContain("Super User", html);
+            }
+
+        }.run();
+    }
+
+    @Test
+    public void testRemovingProviderFromEncounter() throws Exception {
+        final Date date = new Date();
+        new RegressionTestHelper() {
+
+            @Override
+            public void testEditFormHtml(String html) {
+                System.out.println(html);
+            }
+
+            @Override
+            protected String getXmlDatasetPath() {
+                return "org/openmrs/module/htmlformentry19ext/include/";
+            }
+
+            @Override
+            public String getFormName() {
+                return "specifyingEncounterRoleTwiceWithSameRole";
+            }
+
+            @Override
+            public String[] widgetLabels() {
+                return new String[] { "Date:", "Location:", "Doctors:", "Doctors:!!1" };
+            }
+
+            @Override
+            public void setupRequest(MockHttpServletRequest request, Map<String, String> widgets) {
+
+                // first set two providers
+                request.addParameter(widgets.get("Date:"), dateAsString(date));
+                request.addParameter(widgets.get("Location:"), "2");
+                request.addParameter(widgets.get("Doctors:"), "2"); // Doctor Bob
+                request.addParameter(widgets.get("Doctors:!!1"), "1"); // Superuser
+
+            }
+
+            @Override
+            public boolean doEditEncounter() {
+                return true;
+            }
+
+            @Override
+            public String[] widgetLabelsForEdit() {
+                return new String[] { "Date:", "Location:", "Doctors:", "Doctors:!!1" };
+            }
+
+            @Override
+            public void setupEditRequest(MockHttpServletRequest request, Map<String, String> widgets) {
+                // now, in the edit request, only set a single provider
+                request.setParameter(widgets.get("Doctors:"), "1"); // Superuser
+                request.setParameter(widgets.get("Doctors:!!1"), ""); // set the second doctor field blank
+            }
+
+            @Override
+            public void testEditedResults(SubmissionResults results) {
+                results.assertNoErrors();
+                results.assertLocation(2);
+
+                Map<EncounterRole, Set<Provider>> byRoles = results.getEncounterCreated().getProvidersByRoles();
+                Assert.assertEquals(1, byRoles.size());
+
+                Set<Provider> doctors = byRoles.get(Context.getEncounterService().getEncounterRole(3));
+                Assert.assertEquals(1, doctors.size());
+
+                Assert.assertEquals(1, doctors.size());
+                Assert.assertEquals(new Integer(1), doctors.iterator().next().getId());
+            }
+        }.run();
+    }
+
 }
